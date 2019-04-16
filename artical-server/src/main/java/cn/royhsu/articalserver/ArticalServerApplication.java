331d2fe1ed66ef16e15d51592bf0bb25b298c04e
royhsu <616761604@qq.com>
2019-04-16 21:44:39 +0800

    init

---
+++ artical-server/src/main/java/cn/royhsu/articalserver/ArticalServerApplication.java
@@ -0,0 +1,13 @@
+package cn.royhsu.articalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticalServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticalServerApplication.class, args);
+    }
+
+}
