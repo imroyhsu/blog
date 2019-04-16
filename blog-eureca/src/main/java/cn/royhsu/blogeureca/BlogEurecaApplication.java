348be222b7f1de8a64b29181b85e5b04f3b56254
lyc <lyc40761331@126.com>
2019-04-16 23:31:33 +0800

    试试

--- blog-eureca/src/main/java/cn/royhsu/blogeureca/BlogEurecaApplication.java
+++ blog-eureca/src/main/java/cn/royhsu/blogeureca/BlogEurecaApplication.java
@@ -3,7 +3,7 @@ package cn.royhsu.blogeureca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//12345678
 @SpringBootApplication
 @EnableEurekaServer
 public class BlogEurecaApplication {
