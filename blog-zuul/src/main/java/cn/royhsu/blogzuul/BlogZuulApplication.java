39213c0e2f1798532affe8363cf2329ca99c5590
royhsu <616761604@qq.com>
2019-04-18 16:22:37 +0800

    init zuul

---
+++ blog-zuul/src/main/java/cn/royhsu/blogzuul/BlogZuulApplication.java
@@ -0,0 +1,19 @@
+package cn.royhsu.blogzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class BlogZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogZuulApplication.class, args);
+    }
+
+}
