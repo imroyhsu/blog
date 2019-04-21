8d938be5cbd563fd73d26f19f64ff8e88feea0ee
royhsu <616761604@qq.com>
2019-04-21 15:25:07 +0800

    完成日志配置，所有日志可以通过@sl4j形式打印，在admin平台可以看到

--- admin-server/src/main/java/cn/blog/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/cn/blog/adminserver/AdminServerApplication.java
@@ -1,5 +1,7 @@
 package cn.blog.adminserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminServerApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AdminServerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AdminServerApplication.class, args);
     }
@@ -22,6 +26,8 @@ public class AdminServerApplication {
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "roy") String name) {
+        LOG.info("------------hihihihihhi");
+        LOG.debug("dddddddddddddddddddddd");
         return "hi " + name + " ,i am from port:" + port;
     }
 
