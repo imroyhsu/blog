b3be1b15118c6fdeeca1ad48fbec357fcba7cdb7
EthanLiu <lyc40761331@126.com>
2019-04-22 13:48:18 +0800

    添加一些环境

---
+++ article-server/src/main/java/cn/royhsu/articleserver/ArticleServerApplication.java
@@ -0,0 +1,13 @@
+package cn.royhsu.articleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleServerApplication.class, args);
+    }
+
+}
